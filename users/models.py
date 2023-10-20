0f962a9622fcaaed5d3d731d0e8f02f89a027827
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-10-20 18:47:17 +0300

    create User model and apply migrations

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from .managers import CustomUserManager
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name="email address", unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = 'user'
