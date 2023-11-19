86ec23f9c31c54f6e461d3bbaf26ff842750e8f6
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-11-19 15:35:38 +0300

    set settings for deployment to render.com

--- sports_events/settings.py
+++ sports_events/settings.py
@@ -13,7 +13,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv()
+import dj_database_url
+
+ENVIRONMENT = os.getenv('ENV')
+if ENVIRONMENT == 'prod':
+    load_dotenv('.env.prod')
+else:
+    load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,28 +28,28 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0rqld6-pd0(50pe=tqw_b3fwd*2b4lv4s%ygz1@q=xvez#6#12'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
 # Application definition
 
 INSTALLED_APPS = [
-    'users',
-    'core',
-    'events',
-
+    'django.contrib.sessions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
     'django_extensions',
+
+    'core',
+    'users',
+    'events',
 ]
 
 MIDDLEWARE = [
@@ -87,10 +93,7 @@ WSGI_APPLICATION = 'sports_events.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    'default': dj_database_url.config(conn_max_age=600, conn_health_checks=True),
 }
 
 # Password validation
